61b325a5e04aa37f7d8a9ff7740577fcfaf18543
Joachim Fasting <joachim.fasting@gmail.com>
2013-02-12 17:55:16 +0100

    App.Build: make use of Headline instances

--- src/App/Build.hs
+++ src/App/Build.hs
@@ -3,8 +3,7 @@ module App.Build (build) where
 import App.Config
 import Dagbladet.Headline
 
-import Data.Ord (comparing)
-import Data.List (sortBy, nubBy)
+import Data.List (nub, sort)
 
 import qualified Data.ByteString as SB
 import qualified Data.Text as T
@@ -20,7 +19,7 @@ build :: AppConfig -> IO ()
 build cfg = do
   cs <- mapM SB.readFile =<< getChildren (appCacheDir cfg)
   let dest = appDataDir cfg </> "corpus"
-      hl = sortHeadlines $ concatMap parseHeadlines cs
+      hl = sort . nub . concatMap parseHeadlines $ cs
       ot = fmt hl
   T.writeFile dest ot
 
@@ -34,12 +33,6 @@ fmt1 x = record [ hPubDate x, hUrl x, hText x ]
 
 record :: [T.Text] -> T.Text
 record = T.intercalate (T.pack ",")
-
-------------------------------------------------------------------------
-
-sortHeadlines :: [Headline] -> [Headline]
-sortHeadlines = nubBy (\x y -> hText x == hText y)
-              . sortBy (comparing hPubDate)
 
 ------------------------------------------------------------------------
 
