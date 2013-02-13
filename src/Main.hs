3e4723e4b39c52b2c5391c226397a7f4e0017847
Joachim Fasting <joachim.fasting@gmail.com>
2013-02-13 07:57:50 +0100

    Implement headline generator
    Adds a 'gen' command which generates headlines using a markov chain.

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import App.Config
 import App.Get
 import App.Build
+import qualified App.Gen as Gen
 
 import System.Environment (getArgs)
 
@@ -15,3 +16,4 @@ main = do
   case args of
     ("get":_)   -> get conf
     ("build":_) -> build conf
+    ("gen":_)   -> Gen.main conf
